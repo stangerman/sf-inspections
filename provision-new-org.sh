3dc493aa6df8f7a83e56945b2bf8214ec9cd8261
Stephen Tangerman <stangerman@EISTECH-MAC-07FHV2J.local>
2018-08-10 16:01:04 -0500

    fixed a ton of stuff via metadata, yuck

--- provision-new-org.sh
+++ provision-new-org.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 ORG_NAME=$1
-sfdx force:org:create -f config/project-scratch-def.json --setalias $ORG_NAME --setdefaultusername \
+sfdx force:org:create -f config/project-scratch-def.json -a $ORG_NAME username=stephentangermandx@gmail.com --durationdays 30 \
 && sfdx force:source:push \
 && sfdx force:data:tree:import --plan seed-data/seed-data-combined-plan.json \
 && sfdx force:org:open
