e0bfbf164b5db00e97a5fa7639aee05dabfadb38
Stephen Tangerman <stangerman@eistech-mac-07fhv2j.attlocal.net>
2018-08-11 23:41:12 -0500

    expanded data import, erroring on attributes

--- provision-new-org.sh
+++ provision-new-org.sh
@@ -1,6 +1,15 @@
 #! /bin/sh
 ORG_NAME=$1
-sfdx force:org:create -f config/project-scratch-def.json -a $ORG_NAME username=stephentangermandx@gmail.com --durationdays 30 \
-&& sfdx force:source:push \
-&& sfdx force:data:tree:import --plan seed-data/seed-data-combined-plan.json \
-&& sfdx force:org:open
+DOMAIN="user@tangerman.org"
+USERNAME=$ORG_NAME$DOMAIN
+
+echo "********** Org Create - Begin **********"
+sfdx force:org:create -f config/project-scratch-def.json -s -a $ORG_NAME username=$USERNAME --durationdays 30 
+echo "********** Org Create - End **********"
+echo "********** Source Push - Begin **********"
+sfdx force:source:push 
+echo "********** Source Push - End **********"
+echo "********** Data Import - Begin **********"
+sfdx force:data:tree:import --plan ./config/seed-data/seed-data-combined-plan.json 
+echo "********** Data Import - End **********"
+sfdx force:org:open
